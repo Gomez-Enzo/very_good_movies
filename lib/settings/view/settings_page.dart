e7dfd4ba731c307ae4c9bdd1890fc68a7cfc6166
Gomez Enzo <gomezenzo1128@gmail.com>
2022-10-01 15:05:41 -0300

    update

--- lib/settings/view/settings_page.dart
+++ lib/settings/view/settings_page.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies_client/movies_client.dart';
+import 'package:very_good_movies/l10n/l10n.dart';
 import 'package:very_good_movies/settings/cubit/settings_cubit.dart';
 
-class PageSettings extends StatelessWidget {
-  const PageSettings({super.key});
+class ViewSettings extends StatefulWidget {
+  const ViewSettings({
+    super.key,
+    required this.languages,
+  });
+
+  final List<Language> languages;
+
+  static const name = 'settings';
 
   @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => SettingsCubit(),
-      child: const ViewSettings(),
-    );
-  }
+  State<ViewSettings> createState() => _ViewSettingsState();
 }
 
-class ViewSettings extends StatelessWidget {
-  const ViewSettings({super.key});
-
+class _ViewSettingsState extends State<ViewSettings> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,25 +26,52 @@ class ViewSettings extends StatelessWidget {
         title: Text(context.l10n.settings),
         elevation: 4,
       ),
-      body: ListView(
-        children: [
-          ListTile(
-            title: Text(context.l10n.darkMode),
-            trailing: Switch(
-              value: context.watch<SettingsCubit>().state.isDarkMode,
-              onChanged: (value) =>
-                  context.read<SettingsCubit>().toggleDarkMode(),
-            ),
-          ),
-          ListTile(
-            title: Text(context.l10n.darkMode),
-            trailing: Switch(
-              value: context.watch<SettingsCubit>().state.isDarkMode,
-              onChanged: (value) =>
-                  context.read<SettingsCubit>().toggleDarkMode(),
-            ),
-          ),
-        ],
+      body: BlocBuilder<SettingsCubit, SettingsState>(
+        builder: (context, state) {
+          return ListView(
+            children: [
+              ListTile(
+                title: Text(
+                  context.l10n.darkMode,
+                  style: Theme.of(context).textTheme.caption,
+                ),
+                trailing: Switch(
+                  value: state.isDarkMode,
+                  onChanged: (value) {
+                    context.read<SettingsCubit>().toggleDarkMode();
+                  },
+                ),
+              ),
+              ListTile(
+                title: Text(
+                  context.l10n.language,
+                  style: Theme.of(context).textTheme.caption,
+                ),
+                trailing: DropdownButton<String>(
+                  dropdownColor: Theme.of(context).cardColor,
+                  style: Theme.of(context).textTheme.caption,
+                  value: state.language,
+                  onChanged: (value) {
+                    setState(() {
+                      context.read<SettingsCubit>().changeLanguage(value!);
+                    });
+                  },
+                  items: widget.languages
+                      .map(
+                        (language) => DropdownMenuItem(
+                          value: language.iso,
+                          child: Text(
+                            language.englishName,
+                            style: Theme.of(context).textTheme.caption,
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
